1d6f6a8c19f45fed8b3bb538c7f84fc0207f5d87
thesarthakjain <sarthakcrpf456@gmail.com>
2020-10-03 13:34:20 +0530

    bot token saved as env variable 2

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
 
-
+token = os.environ.get("bot_token")
 client = commands.Bot(command_prefix = 'tsj')
 
 @client.event
 async def on_ready():
     print("Bot is alive!")
 
-client.run(bot_token)
+client.run(token)
