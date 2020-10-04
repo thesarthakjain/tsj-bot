5400f8aa3e5d483c76fbd0e336eff874a6b7f42c
thesarthakjain <sarthakcrpf456@gmail.com>
2020-10-04 23:20:19 +0530

    ping command added, prefix changed to .

--- bot.py
+++ bot.py
@@ -4,8 +4,10 @@ import os
 
 token = os.environ.get('bot_token')
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix = '*', intents = intents)
+client = commands.Bot(command_prefix = '.', intents = intents)
 
+
+#events
 @client.event
 async def on_ready():
     print('Bot is working :)')
@@ -17,5 +19,13 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left a server.')
+
+
+#commands
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong bolu kya? :rofl:')
+    await ctx.send(f'{round(client.latency*1000)} ms')
+
 
 client.run(token)
