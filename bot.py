36c8f5b3f25a758eb3e273994540b96708d45dbb
thesarthakjain <sarthakcrpf456@gmail.com>
2020-10-04 23:43:55 +0530

    8ball command added

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import random
 
 token = os.environ.get('bot_token')
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
@@ -26,6 +27,30 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f'Pong bolu kya? :rofl:')
     await ctx.send(f'{round(client.latency*1000)} ms')
+
+@client.command(aliases = ['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['As I see it, yes.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 'Don’t count on it.',
+                 'It is certain.',
+                 'It is decidedly so.',
+                 'Most likely.',
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Outlook good.',
+                 'Reply hazy, try again.',
+                 'Signs point to yes.',
+                 'Very doubtful.',
+                 'Without a doubt.',
+                 'Yes.',
+                 'Yes – definitely.',
+                 'You may rely on it.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
 client.run(token)
