aed4ac3b538a347f27f7211d7d3c3a38bb7f94c6
thesarthakjain <sarthakcrpf456@gmail.com>
2020-10-04 10:19:03 +0530

    OnMemberJoin and OnMemberRemove are working

--- bot.py
+++ bot.py
@@ -3,10 +3,19 @@ from discord.ext import commands
 import os
 
 token = os.environ.get('bot_token')
-client = commands.Bot(command_prefix = 'tsj')
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix = '*', intents = intents)
 
 @client.event
 async def on_ready():
-    print("Bot is alive!!")
+    print('Bot is working!')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
 
 client.run(token)
